2a4dca06f9094b62b721a9717332ef69e32a4c6f
Rafael M Mudafort <rafmudaf@gmail.com>
2019-05-15 15:37:12 -0600

    Use explicit command rather than relying on CMake

--- CTestSteer.cmake
+++ CTestSteer.cmake
@@ -9,7 +9,8 @@ endmacro(getuname)
 getuname(osname -s)
 getuname(cpu    -m)
 
-set(CTEST_BUILD_NAME "${osname}-${COMPILERFLAG}-dev")
+set(CTEST_BRANCH_NAME "dev")
+set(CTEST_BUILD_NAME "${osname}-${COMPILERFLAG}-${CTEST_BRANCH_NAME}")
 set(CTEST_SITE "eagle.hpc.nrel.gov")
 
 ### Environment setup
@@ -18,12 +19,23 @@ set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build")
 
 
 ### Commands
-find_program(CTEST_COMMAND NAMES ctest)
-find_program(CTEST_UPDATE_COMMAND NAMES git)
-find_program(CTEST_CONFIGURE_COMMAND NAMES cmake)
-find_program(CTEST_BUILD_COMMAND NAMES make)
+find_program(CTEST NAMES ctest)
+find_program(GIT NAMES git)
+find_program(CMAKE NAMES cmake)
+find_program(MAKE NAMES make)
 find_program(COMPILER NAMES ${COMPILERFLAG} PATHS ${COMPILERPATH})
-
+set(CTEST_UPDATE_COMMAND "${GIT}")
+set(CTEST_CONFIGURE_COMMAND " \
+  ${CMAKE} \
+  ${CTEST_SOURCE_DIRECTORY} \
+  -DCMAKE_Fortran_COMPILER:PATH=${COMPILERFLAG} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX:PATH=${CTEST_SOURCE_DIRECTORY}/install \
+  -DBUILD_TESTING:BOOL=ON \
+  -DCTEST_REGRESSION_TOL=0.00001"
+)
+set(CTEST_BUILD_COMMAND "${MAKE} beamdyn_driver")
+set(CTEST_COMMAND "${CTEST} -j4")
 
 ### Run CTest
 message(" -- Start ${MODEL} - ${CTEST_BUILD_NAME} --")
@@ -31,58 +43,32 @@ ctest_start(${MODEL} TRACK ${MODEL})
 
 # Update
 message(" -- Update - ${CTEST_BUILD_NAME} --")
-set(CTEST_GIT_INIT_SUBMODULES "ON")
-# ctest_update(
-#   SOURCE "${CTEST_SOURCE_DIRECTORY}"
-#   RETURN_VALUE res
-# )
+ctest_update(RETURN_VALUE res)
 message("ctest_update - number of files updated: ${res}")
 
 # Configure
 message(" -- Configure - ${CTEST_BUILD_NAME} --")
-file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}/reg_tests")  # The turbine model files seem to get corrupted, so copy again every time
-set(options
-  -DCMAKE_Fortran_COMPILER:PATH=${COMPILERFLAG}
-  -DBUILD_TESTING:BOOL=ON
-  -DCMAKE_INSTALL_PREFIX:PATH=${CTEST_SOURCE_DIRECTORY}/install
-  -DCTEST_REGRESSION_TOL=0.00001
-)
-ctest_configure(
-  BUILD "${CTEST_BINARY_DIRECTORY}"
-  SOURCE "${CTEST_SOURCE_DIRECTORY}"
-  OPTIONS "${options}"
-  RETURN_VALUE res
-)
+# file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}/reg_tests")  # The turbine model files seem to get corrupted, so copy again every time
+ctest_configure(RETURN_VALUE res)
 message("ctest_configure - return value of native command: ${res}")
 
-# # Build
-# message(" -- Build - ${CTEST_BUILD_NAME} --")
-# set(CTEST_BUILD_COMMAND "${CTEST_BUILD_COMMAND} -j4")
-# # set(CTEST_BUILD_FLAGS "-j4")
-# set(CTEST_BUILD_TARGET "install")
-# ctest_build(
-#   BUILD "${CTEST_BINARY_DIRECTORY}"
-#   CONFIGURATION Release
-#   RETURN_VALUE res
-# )
-# message("ctest_build - return value of native command: ${res}")
+# Build
+message(" -- Build - ${CTEST_BUILD_NAME} --")
+ctest_build(RETURN_VALUE res)
+message("ctest_build - return value of native command: ${res}")
 
-# # # Test
-# message(" -- Test - ${CTEST_BUILD_NAME} --")
-# ctest_test(
-#   BUILD "${CTEST_BINARY_DIRECTORY}"
-#   # INCLUDE AWT_YFix_WSt
-#   # INCLUDE_LABEL "openfast"
-#   # INCLUDE_LABEL "beamdyn"
-#   # EXCLUDE_LABEL "openfast"
-#   PARALLEL_LEVEL 4 #32  # the 5MW cases fail with -j32 due to multiple threads opening the same file for reading
-#   RETURN_VALUE res
-# )
-# message("ctest_test - 0 if all tests pass: ${res}")
+# Test
+message(" -- Test - ${CTEST_BUILD_NAME} --")
+ctest_test(
+  INCLUDE bd_
+  # INCLUDE_LABEL "openfast"
+  RETURN_VALUE res
+)
+message("ctest_test - 0 if all tests pass: ${res}")
 
-# # # Submit
-# # message(" -- Submit - ${CTEST_BUILD_NAME} --")
-# # # ctest_submit(RETRY_COUNT 20
-# # #              RETRY_DELAY 20
-# # #              RETURN_VALUE res)
-# # message(" -- Finished - ${CTEST_BUILD_NAME} --")
+# Submit
+message(" -- Submit - ${CTEST_BUILD_NAME} --")
+# ctest_submit(RETRY_COUNT 20
+#              RETRY_DELAY 20
+#              RETURN_VALUE res)
+message(" -- Finished - ${CTEST_BUILD_NAME} --")
