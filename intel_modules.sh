429d9540aff7d76f3a94d0dd8687d50e541e21dc
Rafael M Mudafort <rafmudaf@gmail.com>
2019-05-15 16:10:04 -0600

    Bug fix in intel_modules.sh

--- intel_modules.sh
+++ intel_modules.sh
@@ -8,4 +8,4 @@ source activate openfast
 module load comp-intel/2018.0.3
 module load mkl/2018.3.222
 
-export FC=/nopt/nrel/apps/compilers/spack/opt/spack/linux-centos7-x86_64/gcc-4.8.5/intel-parallel-studio-cluster.2018.3-6wq2vvslzhamadvc66fecse5bgcdhjzt/compilers_and_libraries_2018.3.222/linux/bin/int$
+export FC=/nopt/nrel/apps/compilers/spack/opt/spack/linux-centos7-x86_64/gcc-4.8.5/intel-parallel-studio-cluster.2018.3-6wq2vvslzhamadvc66fecse5bgcdhjzt/compilers_and_libraries_2018.3.222/linux/bin/intel64/ifort
